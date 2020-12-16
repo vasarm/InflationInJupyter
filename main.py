2d0ba3667d6e56b7c850fc9e5b5922e9ab3fcbb1
vasarm <vasarmartin0@gmail.com>
2020-12-16 22:31:00 +0200

    page for code documentation

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from modules import InflationModel, Settings
 import numpy as np
 settings = Settings()
 
-model = InflationModel(settings, A="1", B="1", V="x**a")
-model.calculate()
+#model = InflationModel(settings, A="1", B="1", V="x**a")
+#model.calculate()
 
+def test(x):
+    """
+    Testing
+
+    Parameters
+    ----------
+    x : array_like
+        Test
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    return x
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -3,14 +3,14 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Inflation In Jupyter's documentation!
+Inflation In Jupyter's
 ================================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-
+   
+   code
 
 Indices and tables
 ==================
