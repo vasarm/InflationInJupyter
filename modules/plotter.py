c0bd472469b3e5b4220df4319e3382a940bcc6a3
vasarm <vasarmartin0@gmail.com>
2020-12-16 22:01:04 +0200

    Revert "sphinx configuration"
    This reverts commit d8604ca3fb3a601e2e3425f391c2e30240f89ab1.

--- modules/plotter.py
+++ modules/plotter.py
@@ -1,11 +1,3 @@
-import numpy as np
-import sympy as sp
-
-import matplotlib.pyplot as plot_end_values
-
-from . import errors as errors
-
-
 def end_value_selection():
     pass
 def N_integrand_selection():
